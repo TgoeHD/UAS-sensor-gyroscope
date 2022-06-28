1ed64028512b9a48cab145ac02f25fa27bcd7d57
TgoeHD <teguhsatriyo840@gmail.com>
2022-06-28 13:21:56 +0700

    update

--- app/src/main/java/com/example/tugassensorsqlite/MainActivity.java
+++ app/src/main/java/com/example/tugassensorsqlite/MainActivity.java
@@ -17,6 +17,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -25,110 +30,21 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity{
-    TextView xText,yText,zText;
-    Sensor mySensor;
-    SensorManager SM;
-    String SQLiteQuery;
-    Button View;
-    float x,y,z;
-    SQLiteDatabase sqLiteDatabase;
+    Button sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        xText = (TextView) findViewById(R.id.xText);
-        yText = (TextView) findViewById(R.id.yText);
-        zText = (TextView) findViewById(R.id.zText);
-
-        SM = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (mySensor == null){
-            if (SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
-                createDatabase();
-                int MINUTES = 1; // The delay in minutes
-                Timer timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        addData(); // If the function you wanted was static
-                        Handler handler = new Handler(Looper.getMainLooper());
-                        handler.post(
-                                new Runnable()
-                                {
-                                    @Override
-                                    public void run()
-                                    {
-                                        Toast.makeText(MainActivity.this, "Data berhasil ditambah", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                        );
-                    }
-                }, 0, 1000 * 60 * MINUTES);
-                mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            } else{
-                yText.setText("SmartPhone anda tidak mendukung");
-            }
-        }
-
-        View = findViewById(R.id.view);
-        View.setOnClickListener(new android.view.View.OnClickListener() {
+        sensor = findViewById(R.id.sensor);
+        sensor.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ListViewActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
             }
         });
-    }
 
-    public void onResume() {
-        super.onResume();
-        SM.registerListener(accelListener, mySensor,
-                SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    public void onStop() {
-        super.onStop();
-        SM.unregisterListener(accelListener);
-    }
-    SensorEventListener accelListener = new SensorEventListener() {
-        public void onAccuracyChanged(Sensor sensor, int acc) { }
-        @SuppressLint("SetTextI18n")
-        public void onSensorChanged(SensorEvent event) {
-            x = event.values[0];
-            y = event.values[1];
-            z = event.values[2];
-
-            final Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    //Do something after 10 seconds
-                    xText.setText("X: "+(int)x);
-                    yText.setText("Y: "+(int)y);
-                    zText.setText("Z: "+(int)z);
-                    handler.postDelayed(this, 2000);
-                }
-            }, 2000);  //the time is in miliseconds
-        }
-    };
-    private String getCurrentDate() {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault());
-        Date date = new Date();
-
-        return dateFormat.format(date);
-    }
-
-    private void createDatabase() {
-        sqLiteDatabase = openOrCreateDatabase("Nama_Database_Baru", Context.MODE_PRIVATE, null);
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS Nama_Tabel (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR, x VARCHAR, y VARCHAR, z VARCHAR);");
-    }
-
-    private void addData() {
-        SQLiteQuery = "INSERT INTO Nama_Tabel (title,x,y,z) VALUES ('"+ getCurrentDate() +"', '"+ x +"', '"+ y +"', '"+ z +"');";
-        sqLiteDatabase.execSQL(SQLiteQuery);
     }
 }
-// ARBA'I KURNIAJI
-// 19.01.53.0126
-// Pemrograman Aplikasi Mobile B1
